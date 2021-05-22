bb750a39a64971a66891c2c44c7750c16a9b357f
Olha Tarasova <tarasovao0505@gmail.com>
2021-05-22 21:56:52 +0300

    111

--- src/home-work-31.js
+++ src/home-work-31.js
@@ -1,8 +1,7 @@
+"use strict";
+
 const courseTopic = "JavaScript essentials";
-// Change code below this line
 
 const courseTopicLength = courseTopic.length;
 const firstElement = courseTopic[0];
 const lastElement = courseTopic[courseTopic.length - 1];
-
-// Change code above this line
